331ef5cb9074dc3562eb2e4c171721bcba01755d
X - MrG3P5 <57594747+MrG3P5@users.noreply.github.com>
2021-03-17 00:31:01 +0700

    update

--- geps.js
+++ geps.js
@@ -1,53 +1,62 @@
+/*
+* Hanya gabut:v
+* Don't be a script kidde:)
+*/
+
+// MODULE
 const readline = require('readline');
-const brainly = require('brainly-scraper')
-const figlet = require('figlet')
+const BrainlySearch = require('./brainly')
 const chalk = require('chalk');
 
-const rl = readline.createInterface({input : process.stdin,
-                         output : process.stdout});
+// INPUT OUTPUT
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
+// COLOR
 const color = (text, color) => {
     return !color ? chalk.green(text) : chalk.keyword(color)(text)
 }
-
-const soal = 'SOAL: '
-const author = 'X - MrG3P5'
-const soal1 = soal;
+// BANNER
 console.clear()
-console.log(color(figlet.textSync('  BrainlySearch', { horizontalLayout: 'full' }), 'red'))
-console.log('')
-console.log(color('                                      Made by : X - MrG3P5', 'white'))
-console.log(color('                                      Version : 1.0', 'white'))
+console.log(color("..............", 'red'))
+console.log(color("            ..,;:ccc,.", 'red'))
+console.log(color("          ......''';lxO.", 'red'))
+console.log(color(".....''''..........,:ld;", 'red'))
+console.log(color("           .';;;:::;,,.x,", 'red'))
+console.log(color("      ..'''.            0Xxoc:,.  ...", 'red'))
+console.log(color("  ....                ,ONkc;,;cokOdc',.", 'red'))
+console.log(color(" .                   OMo           ':ddo.", 'red'))
+console.log(color("                    dMc               :OO;", 'red'))
+console.log(color("                    0M.                 .:o.", 'red'))
+console.log(color("                    ;Wd", 'red'))
+console.log(color("                     ;XO,", 'red'))
+console.log(color("                       ,d0Odlc;,..", 'red'))
+console.log(color("                           ..',;:cdOOd::,.", 'red'))
+console.log(color("                                    .:d;.':;.", 'red'))
+console.log(color("                                       'd,  .'", 'red'))
+console.log(color("                                         ;l   ..", 'red'))
+console.log(color("                                          .o", 'red'))
+console.log(color("    [=] Made by : X - MrG3P5 [=]            ", 'cyan'), color("c", "red"))
+console.log(color("    [=] Version : 1.0        [=]             ", 'cyan'), color(".'", "red"))
+console.log(color("                                              ", 'cyan'), color(".'", "red"))
 console.log('')
 console.log('')
 
+// USER INPUT QUESTION
 var recursiveAsyncReadLine = function () {
     rl.question('Pertanyaan: ', function (userInput) {
         if (userInput == 'exit' || userInput == 'Exit' || userInput == 'EXIT') {
             console.log(`See you...`)
             process.exit();
-        }                         // angka 5 untuk berapa result yang ingin ditampilkan ke console
-    brainly(userInput.toString(), Number(5)).then((res) => {
-        const brainlyResult = []
-        res.data.forEach((ask) => {
-            const opt = {
-                pertanyaan: ask.pertanyaan,
-                fotoPertanyaan: ask.questionMedia
-            }
-            ask.jawaban.forEach(answer => {
-                opt.jawaban = {
-                    judulJawaban: answer.text,
-                    Author: author
-                }
+        }                          // 5 = Jumlah result
+    BrainlySearch(userInput, Number(5), function (res) {
+            res.forEach(x => {
+                console.log(color(`${x.pertanyaan.replace("Pertanyaan:", "")}\n`, 'green'), color(`Jawaban:`, 'white'), color(`${x.jawaban.judulJawaban.replace("Jawaban:", "")}\n\n`, 'green'))
+                recursiveAsyncReadLine();// CALLBACK INPUT
             })
-            brainlyResult.push(opt)
         })
-        console.log(brainlyResult)
-        recursiveAsyncReadLine();
-    }).catch(err => {
-        console.log(err.error)
-                });
-            });
-        };
-
+    })
+} // CALLBACK INPUT
 recursiveAsyncReadLine();
